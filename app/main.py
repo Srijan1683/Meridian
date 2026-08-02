47da521c5eb7ed938f124fd2207b3315e2fc4dd7
Srijan1683 <pvtsrijan03@gmail.com>
2026-08-02 21:24:13 +0530

    Add netlify origin to CORS config

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://127.0.0.1:5174",
         "http://localhost:5173",
         "http://localhost:5174",
+        "https://meridian-by-srijan.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
